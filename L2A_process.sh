cc9461b54e1b55fb367daca2223f5f714a9e3091
Pierre545 <57266944+Pierre545@users.noreply.github.com>
2023-07-07 10:08:28 +1100

    Create L2A_process.sh

---
+++ L2A_process.sh
@@ -0,0 +1,2 @@
+/home/pierreaudisio/Sen2Cor-02.11.00-Linux64/bin/L2A_Process '/home/pierreaudisio/Documents/test/S2A_MSIL1C_20170402T231851_N0204_R001_T58KDB_20170402T231849.SAFE'
+/home/pierreaudisio/Sen2Cor-02.11.00-Linux64/bin/L2A_Process '/home/pierreaudisio/Documents/test/S2A_MSIL1C_20170422T231851_N0204_R001_T58KDB_20170422T231853.SAFE' 
