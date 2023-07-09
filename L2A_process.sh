73e1b54c60d502d91c81243321d2f843ac2773c5
Pierre545 <57266944+Pierre545@users.noreply.github.com>
2023-07-10 08:42:52 +1100

    L2A_process.sh

--- L2A_process.sh
+++ L2A_process.sh
@@ -1,4 +1,5 @@
-# sudo bash
+# Need to be on super user mode
+# bash command to execute an .sh file
 
 /home/pierreaudisio/Sen2Cor-02.11.00-Linux64/bin/L2A_Process '/home/pierreaudisio/Documents/test/S2A_MSIL1C_20170402T231851_N0204_R001_T58KDB_20170402T231849.SAFE'
 /home/pierreaudisio/Sen2Cor-02.11.00-Linux64/bin/L2A_Process '/home/pierreaudisio/Documents/test/S2A_MSIL1C_20170422T231851_N0204_R001_T58KDB_20170422T231853.SAFE' 
